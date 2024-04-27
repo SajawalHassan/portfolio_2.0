cdcd8abd0ffd985d688531a1cfd3f887d9819417
sajawalhassan <sajawalhassan.1feb@gmail.com>
2024-04-27 14:16:15 +0500

    Add pathway

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
-import { Header } from "@/components/header/header";
+import { Pathway } from "@/components/pathway";
+import { Header } from "@/sections/header";
+import { Hero } from "@/sections/hero";
 
 export default function Home() {
   return (
     <div className="text-white">
       <Header />
+      <main aria-label="main content" className="flex gap-x-[15px]">
+        <Pathway />
+
+        <Hero />
+      </main>
     </div>
   );
 }
---
+++ sections/hero.tsx
@@ -0,0 +1,7 @@
+export const Hero = () => {
+  return (
+    <div className="">
+      <h1>Hello</h1>
+    </div>
+  );
+};
